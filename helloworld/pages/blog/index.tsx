be2b6430171c0708b37c8ced83d5725c52000b2e
Davi5266 <davi.souza09526@gmail.com>
2024-10-18 15:06:03 -0300

    18/10/24-15:05

--- helloworld/pages/blog/index.tsx
+++ helloworld/pages/blog/index.tsx
@@ -1,29 +1,41 @@
 //Entendendo SSG
 import Link from "next/link";
+import { Post } from '../../types/Post';
 
 type Props = {
     name: string;
+    posts: Post[]
 }
 
-const Blog = ({name}: Props) => {
+const Blog = ({name, posts }: Props) => {
 
     return (
         <div>
             <h1>Blog</h1>
             <p>Blog de {name}</p>
-            <li>
-                <Link href="">link</Link>
-            </li>
+            <ul>
+                {posts.map((post, index) => (
+                    <li key={index}>
+                        <Link href={`/blog/${post.id}`}>{post.title}</Link>
+                        </li>
+                ))}
+            </ul>
         </div>
     );
 }
 
-export const getStaticProps = () => {
+export const getStaticProps = async () => {
+    //Requisição a API
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    //const posts = await res.json();
+    const posts: Post[] = await res.json();
 
     return {
         props: {
-            name: 'Bonieky'
-        }
+            name: 'Bonieky',
+            posts
+        },
+        revalidate: 7200
     }
 }
 
